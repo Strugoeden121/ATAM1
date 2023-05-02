fa5d8757b09ce0636f5a38189338392c82b47afa
root <root@LAPTOP-327UPBHI>
2023-05-02 23:36:39 +0300

    ddddd

--- ex3.asm
+++ ex3.asm
@@ -7,33 +7,48 @@ _start:
     lea array2, %r9
     lea mergedArray, %r10
 
-    xor %rax %rax # index arr1
-    xor %rbx %rbx # index arr2
-    xor %rcx %rcx # index merged
+    xor %rax, %rax # index arr1
+    xor %rbx, %rbx # index arr2
+    xor %rcx, %rcx # index merged
     xor %r11d, %r11d # last inputed to merged 
+    xor %r8d, %r8d
+    xor %r12, %r12
 
 
-LOOP_HW3:
-    cmp (%rsi, %rax, 4) , (%r9, %rbx, 4) 
-    jge ARR2_HW3
-    cmp (%rsi, %rax, 4) , %r11d # input from arr1- check if was inputed before
-    je END_ROUND_HW3
+LOOP_HW1:
+    mov (%rsi, %rax, 4), %r8d
+    cmp %r8d , (%r9, %rbx, 4) 
+    jge ARR2_HW1
+    mov %rax, %r12
+    inc %rax
+    cmp (%rsi, %r12, 4) , %r11d # input from arr1- check if was inputed before
+    je END_ROUND_HW1
+    dec %rax
     mov (%rsi, %rax, 4), %r11d 
     mov %r11d, (%r10, %rcx, 4)
     inc %rax
+    inc %rcx
+    jmp END_ROUND_HW1
 
-ARR2_HW3:
-    cmp (%r9, %rbx, 4) , %r11d # input from arr2- check if was inputed before
-    je END_ROUND_HW3
+ARR2_HW1:
+    mov %rbx, %r12
+    inc %rbx
+    cmp (%r9, %r12, 4) , %r11d # input from arr2- check if was inputed before
+    je END_ROUND_HW1
+    dec %rbx
     mov (%r9, %rbx, 4), %r11d 
     mov %r11d, (%r10, %rcx, 4)
     inc %rbx
-
-END_ROUND_HW3:
     inc %rcx
-    cmp $0, (%rsi, %rax, 4)
-    jne LOOP_HW3
-    cmp $0, (%r9, %rbx, 4)
-    jne LOOP_HW3
 
-END_HW3:
+END_ROUND_HW1:
+    xor %r8d, %r8d
+    cmp %r8d, (%rsi, %rax, 4)
+    jne LOOP_HW1
+    xor %r8d, %r8d
+    cmp %r8d, (%r9, %rbx, 4)
+    jne LOOP_HW1
+
+END_HW1:
+    xor %r11d, %r11d
+    mov %r11d, (%r10, %rcx, 4)
