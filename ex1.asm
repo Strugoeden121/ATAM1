f135c64ccce51ed2354e7d499d44d59b7d9e4fb0
Jonah <1jonahg1@gmail.com>
2023-05-02 15:44:46 +0300

    hello

--- ex1.asm
+++ ex1.asm
@@ -2,4 +2,13 @@
 
 .section .text
 _start:
-#your code here
+xor %al, %al
+movq num, %rbx
+loop_HW1:
+shrq $1, %rbx
+jae j_inc_HW1
+inc %al
+j_inc_HW1:
+test %rbx, %rbx
+jnz loop_HW1
+movb %al, Bool
