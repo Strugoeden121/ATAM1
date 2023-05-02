7634b1f3a665c262d8ba0dbe8d409e39b71f25cc
root <root@LAPTOP-H78D8NEV.localdomain>
2023-05-02 17:21:30 +0300

    ex2

--- ex2.asm
+++ ex2.asm
@@ -2,4 +2,26 @@
 
 .section .text
 _start:
-#your code here
+    movl (num) %ebx  # ebx = num 
+    xor %eax, %eax   # eax = i for loop 
+    xor %dx, %dx     # dx = take byte from mem
+    lea source, %rcx   # rcx = mem spot of source 
+    test %ebx, %ebx  
+    js negative_HW2
+
+
+loop_HW2:
+    cmp %eax, %ebx
+    jge end_HW2
+
+    movb (%rcx, %eax), %dx 
+    inc %eax
+    movb %dx, (destination)
+    
+    j loop_HW2
+
+negative_HW2:
+    movl %ebx, (destination)
+  
+
+end_HW2:
